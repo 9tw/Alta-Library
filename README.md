1edc3713969e97a85b97b0de7598193186bc46e8
Gerdo Trada Wijaya <gerdo.tewel@gmail.com>
2022-09-26 16:14:08 +0700

    Add Main

---
+++ README.md
@@ -0,0 +1,18 @@
+# ALTA Library
+
+## About
+ALTA Library is a CLI based library system. There's 3 major flow in this program:
+  1. Register, Login, Edit User.
+  2. See and Search books (no need to Login). Add, Edit, Delete books (Login).
+  3. Borrow book from another User (Login).
+### Built With
+  - Go Language
+  - GORM Library
+  - MySQL Database
+
+## Start
+```bash
+git clone https://github.com/9tw/Alta-Library.git
+```
+
+## Usage
---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func clear() {
+	cmd := exec.Command("clear")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
+
+func main() {
+	var run bool = true
+	var input int
+
+	for run {
+		fmt.Println("--- Welcome to Alta Library ---")
+		fmt.Println("")
+		fmt.Println("1. Search Books")
+		fmt.Println("2. List All Books")
+		fmt.Println("3. Register")
+		fmt.Println("4. Login")
+		fmt.Println("9. Exit")
+		fmt.Println("")
+		fmt.Print("Enter Input: ")
+		fmt.Scan(&input)
+
+		switch input {
+		case 1:
+			var keyword string
+			fmt.Println("--- Search Books in Alta Library ---")
+			fmt.Println("")
+			fmt.Print("Keyword: ")
+			fmt.Scan(&keyword)
+		case 2:
+			fmt.Println("--- List All Books in Alta Library ---")
+			fmt.Println("")
+		case 3:
+			var email string
+			var name string
+			var phone string
+			var address string
+			var password string
+			fmt.Println("--- Register to Alta Library ---")
+			fmt.Println("")
+			fmt.Print("Email: ")
+			fmt.Scan(&email)
+			fmt.Print("Name: ")
+			fmt.Scan(&name)
+			fmt.Print("Phone: ")
+			fmt.Scan(&phone)
+			fmt.Print("Address: ")
+			fmt.Scan(&address)
+			fmt.Print("Password: ")
+			fmt.Scan(&password)
+		case 4:
+			var email string
+			var password string
+			fmt.Println("--- Login to Alta Library ---")
+			fmt.Println("")
+			fmt.Print("Email: ")
+			fmt.Scan(&email)
+			fmt.Print("Password: ")
+			fmt.Scan(&password)
+		case 9:
+			clear()
+			run = false
+			fmt.Println("Bye, see u again.")
+		}
+	}
+}
