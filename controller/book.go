326f40e7abecab5869b2f746b6616bfe55b3d64e
Gerdo Trada Wijaya <gerdo.tewel@gmail.com>
2022-09-28 15:18:17 +0700

    Add, Update, Delete Books

--- controller/book.go
+++ controller/book.go
@@ -6,7 +6,7 @@ type BooksController struct {
 	Model model.BooksModel
 }
 
-func (bc BooksController) GetAll() ([]model.Books, error) {
+func (bc BooksController) GetAll() ([]model.Res, error) {
 	res, err := bc.Model.GetAll()
 	if err != nil {
 		return nil, err
@@ -14,10 +14,34 @@ func (bc BooksController) GetAll() ([]model.Books, error) {
 	return res, nil
 }
 
-func (bc BooksController) Search(key string) ([]model.Books, error) {
+func (bc BooksController) Search(key string) ([]model.Res, error) {
 	res, err := bc.Model.Search(key)
 	if err != nil {
 		return nil, err
 	}
 	return res, nil
+}
+
+func (bc BooksController) AddBook(data model.Books) error {
+	err := bc.Model.AddBook(data)
+	if err != nil {
+		return nil
+	}
+	return nil
+}
+
+func (bc BooksController) UpdateBook(data model.Books) error {
+	err := bc.Model.UpdateBook(data)
+	if err != nil {
+		return nil
+	}
+	return nil
+}
+
+func (bc BooksController) DeleteBook(book_id int) error {
+	err := bc.Model.DeleteBook(book_id)
+	if err != nil {
+		return nil
+	}
+	return nil
 }
