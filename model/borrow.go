52f0970b708b10e3c3950d161b1b366f87dbeaaf
Gerdo Trada Wijaya <gerdo.tewel@gmail.com>
2022-09-28 15:23:32 +0700

    Borrow books feature

---
+++ model/borrow.go
@@ -0,0 +1,59 @@
+package model
+
+import (
+	"fmt"
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Borrows struct {
+	gorm.Model
+	BookID  int
+	UserID  int
+	DueDate time.Time
+}
+
+type Result struct {
+	ID        int
+	BookID    int
+	Title     string
+	UserID    int
+	CreatedAt time.Time
+	DueDate   time.Time
+}
+
+type BorrowsModel struct {
+	DB *gorm.DB
+}
+
+func (bm BorrowsModel) Borrow(newBorrow Borrows) error {
+	err := bm.DB.Exec("INSERT INTO borrows (id, created_at, updated_at, deleted_at, book_id, user_id, due_date) values (?,?,?,?,?,?,?)",
+		nil, time.Now().Format("08-10-1997"), nil, nil, newBorrow.BookID, newBorrow.UserID, (time.Now()).Add(168*time.Hour)).Error
+	er := bm.DB.Exec("UPDATE books SET updated_at = ?, status = ? WHERE id = ?", time.Now(), 1, newBorrow.BookID).Error
+	if err != nil && er != nil {
+		fmt.Println("error on query", err.Error())
+		return nil
+	}
+	return nil
+}
+
+func (bm BorrowsModel) ListBorrow(id int) ([]Result, error) {
+	var res []Result
+	err := bm.DB.Table("borrows").Select("borrows.id", "borrows.created_at", "borrows.book_id", "books.title", "borrows.user_id", "borrows.due_date").Joins("join books on books.id=borrows.book_id").Where("borrows.user_id = ? AND borrows.updated_at IS NULL", id).Model(&Result{}).Find(&res).Error
+	if err != nil {
+		fmt.Println("error on query", err.Error())
+		return nil, err
+	}
+	return res, nil
+}
+
+func (bm BorrowsModel) Return(book_id int, user_id int) error {
+	er := bm.DB.Exec("UPDATE books SET updated_at = ?, status = ? WHERE id = ?", time.Now(), 0, book_id).Error
+	err := bm.DB.Exec("UPDATE borrows SET updated_at = ? WHERE id = ? AND book_id = ? AND updated_at IS NULL", time.Now(), user_id, book_id).Error
+	if err != nil && er != nil {
+		fmt.Println("error on query", err.Error())
+		return nil
+	}
+	return nil
+}
