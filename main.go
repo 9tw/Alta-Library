5592e2ac2879c149be522341757c6e357760b63f
Gerdo Trada Wijaya <gerdo.tewel@gmail.com>
2022-09-29 10:07:25 +0700

    register and login

--- main.go
+++ main.go
@@ -4,7 +4,27 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"main.go/controller"
+	"main.go/model"
 )
+
+func connect() (*gorm.DB, error) {
+	dsn := "root:@tcp(127.0.0.1:3306)/library?parseTime=true"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+	return db, err
+}
+
+func migrate(db *gorm.DB) {
+	db.AutoMigrate(&model.Books{})
+	db.AutoMigrate(&model.Borrows{})
+	db.AutoMigrate(&model.Users{})
+}
 
 func clear() {
 	cmd := exec.Command("clear")
@@ -16,6 +36,18 @@ func main() {
 	var run bool = true
 	var input int
 
+	conn, err := connect()
+	migrate(conn)
+	booksM := model.BooksModel{conn}
+	booksC := controller.BooksController{booksM}
+	borrowsM := model.BorrowsModel{conn}
+	borrowsC := controller.BorrowsController{borrowsM}
+	usersM := model.UsersModel{conn}
+	usersC := controller.UsersController{usersM}
+	if err != nil {
+		fmt.Println("cannot connect to DB", err.Error())
+	}
+
 	for run {
 		fmt.Println("--- Welcome to Alta Library ---")
 		fmt.Println("")
@@ -23,7 +55,7 @@ func main() {
 		fmt.Println("2. List All Books")
 		fmt.Println("3. Register")
 		fmt.Println("4. Login")
-		fmt.Println("9. Exit")
+		fmt.Println("0. Exit")
 		fmt.Println("")
 		fmt.Print("Enter Input: ")
 		fmt.Scan(&input)
@@ -35,15 +67,26 @@ func main() {
 			fmt.Println("")
 			fmt.Print("Keyword: ")
 			fmt.Scan(&keyword)
+			res, err := booksC.Search(keyword)
+			if err != nil {
+				fmt.Println("Some error on get", err.Error())
+			}
+			fmt.Println(res)
 		case 2:
 			fmt.Println("--- List All Books in Alta Library ---")
 			fmt.Println("")
+			res, err := booksC.GetAll()
+			if err != nil {
+				fmt.Println("Some error on get", err.Error())
+			}
+			fmt.Println(res)
 		case 3:
 			var email string
 			var name string
 			var phone string
 			var address string
 			var password string
+			var status string
 			fmt.Println("--- Register to Alta Library ---")
 			fmt.Println("")
 			fmt.Print("Email: ")
@@ -56,6 +99,27 @@ func main() {
 			fmt.Scan(&address)
 			fmt.Print("Password: ")
 			fmt.Scan(&password)
+			fmt.Print("Status: ")
+			fmt.Scan(&status)
+			res, err := usersC.Register(model.Users{
+
+				Model:    gorm.Model{},
+				Email:    email,
+				Name:     name,
+				Phone:    phone,
+				Address:  address,
+				Password: password,
+				Status:   status,
+			})
+			//  res, err := usersC.Register(email, name, phone, address, password, status )
+			if err != nil {
+				fmt.Println("some error on get", err.Error)
+			}
+			fmt.Println(res)
+			fmt.Println("==========================================")
+			fmt.Println("ANDA SUKSES MELAKUKAN PENDAFTARAN")
+			fmt.Println("SELAMAT MENIKMATI FITUR DAN LAYANAN KAMI")
+			fmt.Println("==========================================")
 		case 4:
 			var email string
 			var password string
@@ -65,7 +129,71 @@ func main() {
 			fmt.Scan(&email)
 			fmt.Print("Password: ")
 			fmt.Scan(&password)
-		case 9:
+			res, err := usersC.Search(email, password)
+			if err != nil {
+				fmt.Println("some error on get", err.Error())
+			}
+			fmt.Println(res)
+			// for run {
+			// 	fmt.Println("===========================================")
+			// 	fmt.Println("\tWelcome to Alta Library")
+			// 	fmt.Println("===========================================")
+			// 	fmt.Println("")
+			// 	fmt.Println("1. Edit profile")
+			// 	fmt.Println("2. Non Aktif akun")
+			// 	fmt.Println("3. Pinjam Buku")
+			// 	fmt.Println("4. Liat Buku Yang Dipinjam")
+			// 	fmt.Println("5. Tambah Buku")
+			// 	fmt.Println("6. Edit Buku")
+			// 	fmt.Println("7. Hapus Buku")
+			// 	fmt.Println("0. Exit")
+			// 	fmt.Println("")
+			// 	fmt.Print("Enter Input: ")
+			// 	fmt.Scan(&input)
+
+			// 	switch input {
+			// 	case 1:
+			// 		fmt.Println("==============================")
+			// 		fmt.Println("\tEDIT PROFILE")
+			// 		fmt.Println("==============================")
+
+			// 	case 2:
+			// 		fmt.Println("================================")
+			// 		fmt.Println("\tNON AKTIF AKUN")
+			// 		fmt.Println("================================")
+
+			// 	case 3:
+			// 		fmt.Println("============================")
+			// 		fmt.Println("\tPINJAM BUKU")
+			// 		fmt.Println("============================")
+
+			// 	case 4:
+			// 		fmt.Println("=========================================")
+			// 		fmt.Println("\tLIHAT BUKU YANG DIPINJAM")
+			// 		fmt.Println("=========================================")
+
+			// 	case 5:
+			// 		fmt.Println("============================")
+			// 		fmt.Println("\tTAMBAH BUKU")
+			// 		fmt.Println("============================")
+
+			// 	case 6:
+			// 		fmt.Println("============================")
+			// 		fmt.Println("\tEDIT BUKU")
+			// 		fmt.Println("============================")
+
+			// 	case 7:
+			// 		fmt.Println("============================")
+			// 		fmt.Println("\tHAPUS BUKU")
+			// 		fmt.Println("============================")
+
+			// 	case 0:
+			// 		clear()
+			// 		run = false
+			// 		fmt.Println("Byee")
+			// 	}
+			// }
+		case 0:
 			clear()
 			run = false
 			fmt.Println("Bye, see u again.")
